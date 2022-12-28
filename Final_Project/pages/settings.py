ae8f12b2e2366edd01b58389714a4c22fadc51f9
buryatGod <115992631+buryatGod@users.noreply.github.com>
2022-12-28 17:30:31 +0800

    Create settings.py

---
+++ Final_Project/pages/settings.py
@@ -0,0 +1,33 @@
+class AuthEmail:
+    email = 'ktoskem@gmail.com'
+    password = 'Test12345'
+
+
+class AuthPhone:
+    phone = '9500674244'
+    password = 'Test12345'
+
+
+class AuthLogin:
+    login = 'rtkid_1672218272147'
+    password = 'Test12345'
+
+
+class InvalidData:
+    login = 'login_impostor'
+    password = "Qwerty12345"
+
+
+class SymbolData:
+    login = '@#####@'
+    password = "Qwerty12345"
+
+
+class KirillData:
+    login = 'ПОРУССКИ'
+    password = "ПОРУССКИ"
+
+
+class BIGData:
+    login = 'login'
+    password = "passw"
